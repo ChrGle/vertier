4ffaa23c57d1320c09abeccb0687f5623b629315
ChrGle <christofer.gleissman@vertier.se>
2025-12-03 14:44:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/vertier">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/plattform" element={<Plattform />} />
